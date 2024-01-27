5a2a468c65ab04da1060785c0a5febf2c9a90a20
Shambu Kumar <105148455+The-Merky@users.noreply.github.com>
2024-01-27 06:49:47 +0000

    Implement magic methods Add and Mul for struct Value

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
+use std::ops::{Add, Mul};
 struct Value{
     data:f64,
     children: Vec<Value>
 }
+impl Add for Value{
+    type Output = Value;
+   fn add(self, other: Self) -> Self::Output {
+       Value{data: (self.data+other.data), children: vec![other, self] }
+   } 
+} 
+impl Mul for Value{
+    type Output = Value;
+   fn mul(self, other: Self) -> Self::Output {
+       Value{data: (self.data*other.data), children: vec![other, self] }
+   } 
+} 
 fn main(){
 
 }
