13f253895813068a60bb47a7e06edd00e854606b
Shambu Kumar <105148455+The-Merky@users.noreply.github.com>
2024-02-01 14:06:06 +0000

    Implement basic constructor for struct Layer

--- src/layer.rs
+++ src/layer.rs
@@ -1,15 +1,34 @@
-use nalgebra::{DVector, DMatrix};
+use nalgebra::{DMatrix, DVector};
 /*
 Layer struct for neural networks
 Contains a vector of weights and a vector of biasesstruct Layer<T: nalgebra::Dim>
 */
-struct Layer<T>
-{
-    weights: DMatrix<T>,
-    biases: DVector<T>,
-    activation: fn(T) -> T
+enum ActivationFunction {
+    Sigmoid,
+    Relu,
+    Tanh,
+    Softmax,
 }
-
-
-
-
+struct Layer {
+    weights: DMatrix<f64>,
+    biases: DVector<f64>,
+    activation_fn: ActivationFunction,
+}
+impl Layer {
+    fn new(size: usize, is_input: bool, activation_function: ActivationFunction) -> Layer {
+        if (is_input) {
+            Layer {
+                weights: DMatrix::from_element(size, size, 1.0),
+                biases: DVector::from_element(size, 1.0),
+                activation_fn: activation_function,
+            }
+        } else {
+            //TODO: Create Layer with random weights and biases
+           Layer{
+                weights: DMatrix::from_element(size, size, 1.0),
+                biases: DVector::from_element(size, 1.0),
+                activation_fn: activation_function,
+           }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 pub mod layer;
 
 fn main() {
- 
 }
