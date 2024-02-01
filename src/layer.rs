cfa932c3b54929e759b178e3332ce5144d360dfa
Shambu Kumar <105148455+The-Merky@users.noreply.github.com>
2024-02-01 13:21:27 +0000

    Fix error: Switch to DVector and DMatrix to be able to determine size at construction

--- src/layer.rs
+++ src/layer.rs
@@ -1,21 +1,15 @@
-
+use nalgebra::{DVector, DMatrix};
 /*
 Layer struct for neural networks
 Contains a vector of weights and a vector of biasesstruct Layer<T: nalgebra::Dim>
 */
-struct Layer<T : nalgebra::DimName>
-
-where
-    T: nalgebra::DimName,
+struct Layer<T>
 {
-    weights: SVector<f32, T>,
-    biases: SVector<f32, T>,
+    weights: DMatrix<T>,
+    biases: DVector<T>,
+    activation: fn(T) -> T
 }
 
-impl<T: nalgebra::DimName> Layer<T>
-where
-    T: nalgebra::DimName,
-{
-    fn new(weights: SVector<f32, T>, biases: SVector<f32, T>) -> Self { Self { weights, biases } }
-} 
+
+
 
