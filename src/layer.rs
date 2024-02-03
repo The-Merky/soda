62595450f4b33e8f30631f87834c22187e45c58d
Shambu Kumar <105148455+The-Merky@users.noreply.github.com>
2024-02-03 11:13:59 +0000

    Complete TODO: Create Layer with random weights and biases

--- src/layer.rs
+++ src/layer.rs
@@ -1,33 +1,39 @@
 use nalgebra::{DMatrix, DVector};
+use rand::Rng;
 /*
 Layer struct for neural networks
 Contains a vector of weights and a vector of biasesstruct Layer<T: nalgebra::Dim>
 */
-enum ActivationFunction {
+pub enum ActivationFunction {
     Sigmoid,
     Relu,
     Tanh,
     Softmax,
 }
-struct Layer {
+pub struct Layer {
     weights: DMatrix<f64>,
     biases: DVector<f64>,
     activation_fn: ActivationFunction,
-}
+    //Result of activation function
+    pub activation_result: DVector<f64>,
+} 
 impl Layer {
-    fn new(size: usize, is_input: bool, activation_function: ActivationFunction) -> Layer {
+    pub fn new(size: usize, is_input: bool, activation_function: ActivationFunction) -> Layer {
         if (is_input) {
             Layer {
                 weights: DMatrix::from_element(size, size, 1.0),
                 biases: DVector::from_element(size, 1.0),
                 activation_fn: activation_function,
+                activation_result :DVector::from_element(size, 1.0),
             }
         } else {
-            //TODO: Create Layer with random weights and biases
-           Layer{
-                weights: DMatrix::from_element(size, size, 1.0),
+            let mut rng = rand::thread_rng();
+            Layer{
+            //Random weights between 1 and -1
+                weights: DMatrix::from_fn(size, size, |_, _| rng.gen_range(-1.0..1.0)),
                 biases: DVector::from_element(size, 1.0),
                 activation_fn: activation_function,
+            activation_result: DVector::from_element(size, 1.0),
            }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 pub mod layer;
-
+use nalgebra;
 fn main() {
+    let mut layer = layer::Layer::new(2, true, layer::ActivationFunction::Sigmoid);
+
+
 }
