3d04588868554db1af4700fff9eb3cd34bda3ce4
Shambu Kumar <kumarshambu2010@gmail.com>
2024-02-06 07:30:14 +0100

    Fix error messages in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::ops::Mul;
 
-pub mod neural_net;
 pub mod layer;
+pub mod neural_net;
 fn main() {
-    let layer = layer::Layer::new(2, layer::ActivationFunction::Sigmoid, 1);
-    let layer2 = layer::Layer::new(2, layer::ActivationFunction::Sigmoid, 2);
-    let net = neural_net::NeuralNet::new();
+    return;
 }
